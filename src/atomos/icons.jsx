ecbaded97733f5260a721fa326cc6965e28580dd
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-10-23 16:27:18 -0600

    modified:   src/atomos/icons.jsx new file:   src/img/toro.svg 	new file:   src/img/vaca.svg 	src/atomos/svg.css 	src/pages/

--- src/atomos/icons.jsx
+++ src/atomos/icons.jsx
@@ -6,15 +6,26 @@ import {BiLogOut}from "react-icons/bi";
 
 
 
+import "./svg.css"
 function Icons(props) {
     const {iconName}=props;
+
 
     Icons={
         "home":<BsFillHouseFill/>,
         "tag":<AiFillTag/>,
         "agregar":<BsPlusCircleFill/>,
         "graficas":<BsGraphUpArrow/>,
-        "salir":<BiLogOut/>
+        "salir":<BiLogOut/>,
+        "documentos":<IoDocumentText/>,
+        "camara":<BsCameraFill/>,
+        "cerrar":<AiFillCloseCircle/>,
+        "eliminar":<BsTrashFill/>,
+        "descargar":<BsDownload/>,
+        "guardar":<FaSave/>,
+        "editar":<BsPencilSquare/>,
+        "vaca": <img src={Vaca} alt="vaca" />,
+        "toro": <img src={Toro} alt="toro" />
     }
 
     return(
