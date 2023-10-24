df71f9bbcfabefff50003149486fb4692f7d173d
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-10-23 20:30:22 -0600

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals.js';
-/* import Leftvar from "./componentes/leftVar.jsx"
- */
 import ModalModificarPerfil from './componentes/modalModificarPerfil.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
