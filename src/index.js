ea58c7f39f60bf05c86facca76b478924e687f34
ATRIZEL203 <darkangel_066@hotmail.com>
2023-10-28 03:00:36 -0600

    no message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals.js';
-/* import Leftvar from "./componentes/leftVar.jsx"
- */
-import Calendario from './componentes/modalCalendar.jsx';
+import Perfil from './componentes/modalP1.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <div> 
-    <Calendario />
+    <Perfil />
   </div>
   </React.StrictMode>
 );
