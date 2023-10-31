c927faae51c7d33f2c8f351355693f655f8d4eac
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-10-31 11:11:00 -0600

    modified:   src/atomos/botonCirculo.jsx 	modified:   src/css/botonCirculo.css 	modified:   src/css/perfilChico.css 	modified:   src/pages/primerFiltroV.jsx

--- src/atomos/botonCirculo.jsx
+++ src/atomos/botonCirculo.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "../css/botonCirculo.css";
+
 function BotonCirculo(props) {
+    const buttonStyle = {
+        backgroundColor: props.style.backgroundColor || "#5AA257",  
+        color: props.style.color || "#FFFFFF", 
+    };
+
     return (
-        <div className="botonCirculo" onClick={props.onClick}>
+        <div className="botonCirculo" onClick={props.onClick} style={buttonStyle}>
             <span>{props.texto}</span>
         </div>
     );
