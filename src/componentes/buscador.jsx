cc1bd877500e094bcb8e5239b5fc75722f5b2737
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-11-16 20:16:18 -0600

    Add filtering functionality to search bar component

--- src/componentes/buscador.jsx
+++ src/componentes/buscador.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import "../css/buscador.css"
+import React, { useState } from 'react';
+import "../css/buscador.css";
 import Icons from '../atomos/icons';
 
-function buscador(props) { 
+function Buscador(props) { 
+    const [filtradoNombre, setFiltrado] = useState([]);
+    const arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];  
+
+    const handleChange = ({ target }) => {
+        const filtered = arr.filter((nombre) => nombre.includes(target.value));
+        setFiltrado(filtered);
+    };
+    
     return (
         <form className="form">
             <button>
-                    <Icons iconName="buscar"/>
+                <Icons iconName="buscar"/>
             </button>
-            <input className="input" placeholder="Escribe el Nombre o el Arete" required type="text" />
+            <input className="input" placeholder="Escribe el Nombre o el Arete" required type="text" onChange={handleChange} list='x'  />
             <button className="reset" type="reset">
                 <Icons iconName="cerrar"/>
             </button>
+            <datalist id='x'>
+                {filtradoNombre.map((nombre, index) => (
+                    <option key={index} value={nombre} />
+                ))}
+            </datalist>
         </form>
     );
 }
 
-
-export default buscador;
+export default Buscador;
