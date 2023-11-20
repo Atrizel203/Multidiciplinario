10c0c9de8fc680f24fa598ca4c9c2c403c7e9c8a
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-11-19 22:07:21 -0600

    Add Buscador component to primerFiltroV.jsx

--- src/pages/primerFiltroV.jsx
+++ src/pages/primerFiltroV.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LeftVar from "../componentes/leftVar.jsx";
 import MiniPerfiles from "../componentes/modalP1.jsx";
 import BotonCirculo from "../atomos/botonCirculo.jsx";
+import Buscador from "../componentes/buscador.jsx";
 import "./css/primerFiltro.css";
 
 function PerfilVaca() {
@@ -19,7 +20,7 @@ function PerfilVaca() {
         { arete: "1234", nombreDeVaca: "1234" },
         { arete: "1234", nombreDeVaca: "1234" },
         { arete: "1234", nombreDeVaca: "1234" },
-        { arete: "1234", nombreDeVaca: "1234" },   
+        { arete: "1234", nombreDeVaca: "1234" },
     ];
 
     const perPage = 6;
@@ -28,26 +29,28 @@ function PerfilVaca() {
     const totalPages = Math.ceil(profiles.length / perPage);
 
     return (
-        <div className="contenedor-principal">
+        <div>
             <LeftVar />
-            <div className="organismVacas">
-                <div className="contenido-derecho">
-                    {profiles
-                        .slice((currentPage - 1) * perPage, currentPage * perPage)
-                        .map((profile, index) => (
-                            <MiniPerfiles key={index} arete={profile.arete} nombreDeVaca={profile.nombreDeVaca} />
+            <Buscador />
+            <div className="contenedor-principal">
+                <div className="organismVacas">
+                    <div className="contenido-derecho">
+                        {profiles
+                            .slice((currentPage - 1) * perPage, currentPage * perPage)
+                            .map((profile, index) => (
+                                <MiniPerfiles key={index} arete={profile.arete} nombreDeVaca={profile.nombreDeVaca} />
+                            ))}
+                    </div>
+                    <div className="pagination">
+                        {[...Array(totalPages).keys()].map((page) => (
+                            <BotonCirculo
+                                key={page}
+                                texto={page + 1}
+                                onClick={() => setCurrentPage(page + 1)}
+                                style={page + 1 === currentPage ? { backgroundColor: "#FFFFFF", color: "#5AA257" } : {}}
+                            />
                         ))}
-                </div>
-
-                <div className="pagination">
-                    {[...Array(totalPages).keys()].map((page) => (
-                        <BotonCirculo
-                            key={page}
-                            texto={page + 1}
-                            onClick={() => setCurrentPage(page + 1)}
-                            style={page + 1 === currentPage ? { backgroundColor: "#FFFFFF", color: "#5AA257" } : {}}
-                        />
-                    ))}
+                    </div>
                 </div>
             </div>
         </div>
