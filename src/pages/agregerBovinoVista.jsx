768054a69abe74381fe9582ef5ec1ba9a910b661
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-11-22 01:49:59 -0600

    Add functionality to create a new bovino entity

--- src/pages/agregerBovinoVista.jsx
+++ src/pages/agregerBovinoVista.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import LeftVar from "../componentes/leftVar.jsx"
 import AgregarBovino from "../componentes/agregarBovino.jsx"
 import Buscador from "../componentes/buscador";
-import obtenerDatosApi from "../api/buscador.js";
 function agregarBovinoVista(params) {
-    /* AQUI ES NECESARIO PASAR EL URL DEL LA DB QUE ESTA EN LA NUBE QUE LE PERTENECE AL USUARIO*/
-    const dblink = "http://localhost:3003";/* ejemplo */
-    const dataBovinosAll = obtenerDatosApi( dblink );
-    
 
     return (
         <div >
             <LeftVar />
-            <Buscador infoBovinos={dataBovinosAll} />
+            <Buscador />
             <div className="modalSpaw">
                 <AgregarBovino />
             </div>
