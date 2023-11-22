316912cf499372f6961fbd3ee4051c673bb4b405
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-11-22 07:49:04 -0600

    Fix date input bug in ModalAgregarEvento component

--- src/componentes/modalAgregarEvento.jsx
+++ src/componentes/modalAgregarEvento.jsx
@@ -4,22 +4,25 @@ import BotonGuardar from "../atomos/botonGuardar.jsx";
 import ImputAgregar from "../moleculas/impustEspeciales.jsx";
 import Style from "../css/modalAgregarEvento.module.css";
 import swal from 'sweetalert';
+import crearEventos from '../api/crearEntidades/crearEvento.js';
 
 function ModalAgregarEvento({ onClose }) {
     const [modalIsOpen, setModalIsOpen] = useState(true);
     const [tituloEvento, setTituloEvento] = useState("");
     const [asunto, setAsunto] = useState("");
     const [descripcion, setDescripcion] = useState("");
-    const [fechaNacimiento, setFechaNacimiento] = useState("");
+    const [fechaTerminar, setFechaNacimiento] = useState("");
     const [tituloFeedback, setTituloFeedback] = useState("");
     const [asuntoFeedback, setAsuntoFeedback] = useState("");
     const [fechaFeedback, setFechaFeedback] = useState("");
 
     const closeModal = () => {
-      if (onClose) {
-        onClose();
-      }
+        if (onClose) {
+            onClose();
+        }
     };
+
+
 
     let opcionesEvento = [
         { value: '', label: '' },
@@ -32,25 +35,39 @@ function ModalAgregarEvento({ onClose }) {
     ];
 
     const handleGuardar = () => {
-        if (tituloEvento === "" || asunto === "" || fechaNacimiento === "") {
+        if (tituloEvento === "" || asunto === "" || fechaTerminar === "") {
             if (tituloEvento === "") {
                 setTituloFeedback("¡Requiere rellenar el título del evento!");
             }
             if (asunto === "") {
                 setAsuntoFeedback("¡Requiere seleccionar un asunto!");
             }
-            if (fechaNacimiento === "") {
+            if (fechaTerminar === "") {
                 setFechaFeedback("¡Requiere seleccionar una fecha!");
             }
             swal({
                 title: "Por favor, rellena los campos importantes antes de guardar.",
                 icon: "error",
-              });
+            });
         } else {
+
+            let eventoObetnido = {
+                "idBovino": localStorage.getItem("idBovino"),
+                "tituloEvento": tituloEvento,
+                "asunto": asunto,
+                "descripcion": descripcion,
+                "fechaTerminar": fechaTerminar,
+                "created_bySub": localStorage.getItem("correo"),
+                "fecha_Reporte": new Date().toISOString()
+            }
+
+            crearEventos(eventoObetnido);
+
+            console.log(eventoObetnido);
             swal({
                 title: "Los datos se han guardado correctamente.",
                 icon: "success",
-              });
+            });
         }
     };
 
@@ -63,7 +80,7 @@ function ModalAgregarEvento({ onClose }) {
                     <Icons iconName="calendario" />
                 </h1>
                 <div onClick={closeModal}>
-                <Icons iconName="cerrar" />
+                    <Icons iconName="cerrar" />
                 </div>
             </div>
 
@@ -72,8 +89,8 @@ function ModalAgregarEvento({ onClose }) {
             <ImputAgregar texto="Asunto" tipo="select" options={opcionesEvento} value={asunto} onChange={setAsunto} />
             {!asunto && <div className="feedback">{asuntoFeedback}</div>}
             <ImputAgregar texto="Descripcion" tipo="textarea" className={Style.descripcion} value={descripcion} onChange={setDescripcion} />
-            <ImputAgregar texto="Fecha A Terminar" tipo="date" value={fechaNacimiento} onChange={setFechaNacimiento}/>
-            {!fechaNacimiento && <div className="feedback">{fechaFeedback}</div>}
+            <ImputAgregar texto="Fecha A Terminar" tipo="date" value={fechaTerminar} onChange={setFechaNacimiento} />
+            {!fechaTerminar && <div className="feedback">{fechaFeedback}</div>}
             <div className={Style.guardar} onClick={handleGuardar}>
                 <BotonGuardar />
             </div>
