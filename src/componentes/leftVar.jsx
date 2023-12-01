73c70121f98d6eb34c3da76b46b7f83c31f15593
Norhan15 <normamarlithsm15@gmail.com>
2023-12-01 08:24:02 -0600

    Arreglos y modal filtro

--- src/componentes/leftVar.jsx
+++ src/componentes/leftVar.jsx
@@ -23,6 +23,8 @@ import Eventos from "@mui/icons-material/EventNote";
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import Logo from '../img/logoNuevo.png';
+import "../css/leftvar.css"
+import { Checkbox} from '@mui/material';
 
 
 function Leftvar(props) {
@@ -86,13 +88,13 @@ function Leftvar(props) {
 
             {/* Menú lateral */}
             <Hidden mdDown>
-                <Drawer variant="permanent" anchor="left" open>
+                <Drawer /* PaperProps={{ style: { width: '210px' } }}  agregar dependiendo el ancho de la pantalla*/ variant="permanent" anchor="left" open>
                     <Box sx={{ p: 2 }}>
                         <div className="logoNombre">
                             <img className="Logonuevo2" src={Logo} alt="Logo" />
                             <Typography variant="h6" className="Bovinsoft">
                                 BOVINSOFT
-                            </Typography>
+                           </Typography>
                         </div>
                     </Box>
                     <List>
@@ -137,65 +139,95 @@ function Leftvar(props) {
             </Hidden>
 
             {/* Modal de Estados Bovinos */}
-            <Modal open={modalOpen} onClose={toggleModal}>
-                <Box
-                    sx={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        width: 300,
-                        bgcolor: 'background.paper',
-                        boxShadow: 24,
-                        p: 2,
-                    }}
-                >
-                    <Typography variant="h6" gutterBottom>
-                        Estados Bovinos
-                    </Typography>
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.cargada} onChange={() => handleSwitchChange('cargada')} />}
-                        label="Cargada"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.enferma} onChange={() => handleSwitchChange('enferma')} />}
-                        label="Enferma"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.esperInseminar} onChange={() => handleSwitchChange('esperInseminar')} />}
-                        label="Esper Inseminar"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.nacimientoEsp} onChange={() => handleSwitchChange('nacimientoEsp')} />}
-                        label="Nacimiento Esp"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.lecionada} onChange={() => handleSwitchChange('lesionada')} />}
-                        label="Lesionada"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.fueraFinca} onChange={() => handleSwitchChange('fueraFinca')} />}
-                        label="Fuera Finca"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.favorito} onChange={() => handleSwitchChange('favorito')} />}
-                        label="Favorito"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.vendido} onChange={() => handleSwitchChange('vendido')} />}
-                        label="Vendido"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={bovineStates.muerta} onChange={() => handleSwitchChange('muerta')} />}
-                        label="Muerta"
-                    />
-                    <Button variant="contained" onClick={saveBovineStates}>
-                        Buscar
-                    </Button>
-                </Box>
-            </Modal>
+           
+<Modal open={modalOpen} onClose={toggleModal}>
+    <Box
+        sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 500,
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 2,
+        }}
+    >
+        <Typography variant="h6" gutterBottom sx={{ color:"#8FD182" }}>
+            Estados Bovinos
+        </Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <Box sx={{ flexGrow: 1, bgcolor: '#4C9680', height: '2px' }} />
+            <Typography variant="subtitle1" sx={{ color:"#4C9680 ", mx: 2 }}>
+                Estados de Salud
+            </Typography>
+            <Box sx={{ flexGrow: 1, bgcolor: '#4C9680', height: '2px' }} />
+        </Box>
+        
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.enferma} onChange={() => handleSwitchChange('enferma')} />}
+                label="Enferma"
+            />
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.lecionada} onChange={() => handleSwitchChange('lecionada')} />}
+                label="Lecionada"
+            />
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.muerta} onChange={() => handleSwitchChange('muerta')} />}
+                label="Muerta"
+            />
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <Box sx={{ flexGrow: 1, bgcolor: '#4C9680', height: '2px' }} />
+            <Typography variant="subtitle1" sx={{ color:"#4C9680 ", mx: 2 }}>
+            Estados de Fertilización
+            </Typography>
+            <Box sx={{ flexGrow: 1, bgcolor: '#4C9680', height: '2px' }} />
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.cargada} onChange={() => handleSwitchChange('cargada')} />}
+                label="Cargada"
+            />
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.esperInseminar} onChange={() => handleSwitchChange('esperInseminar')} />}
+                label="Esper Inseminar"
+            />
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.nacimientoEsp} onChange={() => handleSwitchChange('nacimientoEsp')} />}
+                label="Nacimiento Esp"
+            />
+        </Box>
+
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <Box sx={{ flexGrow: 1, bgcolor: '#4C9680', height: '2px' }} />
+            <Typography variant="subtitle1" sx={{ color:"#4C9680 ", mx: 2 }}>
+                Otros estados
+            </Typography>
+            <Box sx={{ flexGrow: 1, bgcolor: '#4C9680', height: '2px' }} />
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.fueraFinca} onChange={() => handleSwitchChange('fueraFinca')} />}
+                label="Fuera Finca"
+            />
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.favorito} onChange={() => handleSwitchChange('favorito')} />}
+                label="Favorito"
+            />
+            <FormControlLabel
+                control={<Checkbox checked={bovineStates.vendido} onChange={() => handleSwitchChange('vendido')} />}
+                label="Vendido"
+            />
+        </Box>
+
+        <Button sx={{  margin: 'auto' }} variant="contained" onClick={saveBovineStates}>
+            Guardar
+        </Button>
+    </Box>
+</Modal>
         </div>
     );
 }
-
 export default Leftvar;
